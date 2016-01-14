ab7bba459df90df03ca22b0245496fc3f72ce383
Cody Bouscaren <codybousc@gmail.com>
2016-01-14 16:18:00 -0500

    Modal Error Corrections

--- controllers/CamperNewsController.js
+++ controllers/CamperNewsController.js
@@ -2,18 +2,34 @@ stylingCampersStoriesApp = angular.module("stylingCampersStoriesApp", ['ui.boots
 
 stylingCampersStoriesApp.controller('CamperNewsController', ['$scope','$http', '$uibModal', '$log',
 function($scope, $http, $uibModal, $log) {
+  $scope.allData;
+  $scope.allPostsArray = [];
 
   $http({
   method: 'GET',
   url: 'http://www.freecodecamp.com/news/hot'
 }).then(function successCallback(response) {
     $scope.allData = response.data;
+    for(var i = 0; i < $scope.allData.length; i++) {
+      $scope.allPostsArray.push($scope.allData[i])
+    }
   }, function errorCallback(response) {
     console.log(response);
   });
 
+  $scope.findPostById = function(postId) {
+    // console.log($scope.PostsArray);
+    for(var i = 0; i < $scope.allPostsArray.length; i++) {
+      if(postId == $scope.allPostsArray[i].id) {
+        return $scope.allPostsArray[i];
+        break
+      }
+    }
+  }
+
 //==========================================================
 //open modal
+  $scope.items = ['item1', 'item2', 'item3'];
 
   $scope.animationsEnabled = true;
 
